6e68bac4ba53b1f589b994a5e30b079eafd96262
skuzema <s.kuzema@gmail.com>
2023-04-17 19:42:23 +0300

    fix: get search value from store

--- 05_redux/src/components/forms/searchBar/SearchBar.tsx
+++ 05_redux/src/components/forms/searchBar/SearchBar.tsx
@@ -3,15 +3,15 @@ import { useForm } from 'react-hook-form';
 import PropTypes from 'prop-types';
 
 import { SearchParams } from '../../../models/IArticles';
-import { useAppDispatch } from '../../../store/hooks';
+import { useAppDispatch, useAppSelector } from '../../../store/hooks';
 
 import './SearchBar.scss';
 import { setQueryParam } from '../../../store/reducers/queryParamSlice';
 
 const SearchBar = () => {
   const { register, handleSubmit } = useForm<SearchParams>({ mode: 'onSubmit' });
-
   const dispatch = useAppDispatch();
+  const qry = useAppSelector((state) => state.queryParam.q);
 
   const onFormSubmit = (par: SearchParams) => {
     dispatch(
@@ -23,7 +23,7 @@ const SearchBar = () => {
 
   return (
     <form onSubmit={handleSubmit(onFormSubmit)} className="search-conatiner">
-      <input {...register('q')} className="search-bar" placeholder="Title" defaultValue="" />
+      <input {...register('q')} className="search-bar" placeholder="Title" defaultValue={qry} />
       <input type="submit" className="btn-search" value="Search" />
     </form>
   );
